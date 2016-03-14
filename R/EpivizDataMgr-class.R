2c89c1c2812c2a2f24e5598be9af2068dc7d9782
Hector Corrada Bravo <hcorrada@gmail.com>
2016-03-14 16:27:44 -0400

    remove type map

--- R/EpivizDataMgr-class.R
+++ R/EpivizDataMgr-class.R
@@ -4,19 +4,10 @@
 #' @import methods
 #' @importClassesFrom epivizrServer EpivizServer
 #' 
-#' @details
-#' The 'register_type' method is used to register data types to be handled by the
-#' manager. Usage is as follows:
-#' \code{register_type(type_name, type_descriptor)}: with \code{type_name} a character
-#' string specifying the type name and \code{type_descriptor} a list with slots
-#' \code{class} specifying with \code{EpivizData} subclass it corresponds to,
-#' \code{description} describing the data type and 
-#' \code{input_class} specifying which object type is taken as input
 #' 
 EpivizDataMgr <- setRefClass("EpivizDataMgr",
   fields = list(
     .ms_list = "list",
-    .type_map = "list",
     .ms_idCounter = "integer",
     .server = "EpivizServer"
   ),
@@ -24,7 +15,6 @@ EpivizDataMgr <- setRefClass("EpivizDataMgr",
     initialize=function(server, ...) {
       .self$.server <- server
       .self$.ms_list <- vector("list")
-      .self$.type_map <- vector("list")
       .self$.ms_idCounter <- 0L
     },
     show = function() {
@@ -41,16 +31,6 @@ EpivizDataMgr <- setRefClass("EpivizDataMgr",
     is_server_closed = function() {
       "Check if underlying server is closed, <logical>"
       .server$is_closed()
-    },
-    register_type=function(type_name, type_descriptor) {
-      "Register a data type to the manager (See Details)"
-      if (is.null(type_descriptor$class) ||
-          is.null(type_descriptor$description) ||
-          is.null(type_descriptor$input_class))
-        stop("type_descriptor is not valid see ?`EpivizDataMgr-class`")
-      
-      .self$.type_map[[type_name]] <- type_descriptor
-      .self$.ms_list[[type_name]] <- new.env()
     }
   )
 )
--- tests/testthat/test-creation.R
+++ tests/testthat/test-creation.R
@@ -25,22 +25,3 @@ test_that("server opening works as expected", {
   expect_false(mgr$is_server_closed())
 })
 
-test_that("type registration works", {
-  server <- epivizrServer::createServer()
-  mgr <- EpivizDataMgr$new(server=server)
-  
-  descriptor <- list(class="EpivizData",
-    description="A phony data type",
-    input_class="GRanges")
-
-  mgr$register_type("data_type", descriptor)
-  expect_equal(length(mgr$.ms_list), 1)
-  expect_false(is.null(mgr$.ms_list[["data_type"]]))
-  expect_is(mgr$.ms_list[["data_type"]], "environment")
-  
-  expect_equal(length(mgr$.type_map), 1)
-  expect_false(is.null(mgr$.type_map[["data_type"]]))
-  expect_equal(mgr$.type_map[["data_type"]], descriptor)
-  
-  expect_error(mgr$register_type("another_type", list()))
-})
