91344990892ce888de465782f234707b7eb4c4b6
Hector Corrada Bravo <hcorrada@gmail.com>
2016-04-01 10:15:10 -0400

    rm_allMeasurements tested with connection

--- tests/testthat/test-connected_management.R
+++ tests/testthat/test-connected_management.R
@@ -156,21 +156,50 @@ test_that("rm_measurements works without connection", {
 })
 
 test_that("rm_allMeasurements works without connection", {
-  skip("for now")
+  skip_on_cran()
+  skip_on_os("windows")
+  skip_if_not_installed("RSelenium")
+  
+  if (!.canPhantomTest()) {
+    skip("This test can't be run in this environment")
+  }
+  
+  server <- epivizrServer::createServer(port=7123L, daemonized=TRUE, verbose=TRUE)
+  if (!server$is_daemonized()) {
+    skip("This test only works for daemonized servers")
+  }
+  
+  .startRemoteDriver()
+  on.exit({cat("stopping remDr\n"); .stopPhantomJS()})
+  
+  server$start_server(static_site_path=".")
+  on.exit({cat("stopping server\n"); server$stop_server()}, add=TRUE)
+  
+  mgr <- createMgr(server)
+  .navigateRemoteDriver(port=server$.port)
+  wait_until(mgr$.server$is_socket_connected())
+  
   gr1 <- GRanges(seqnames="chr1", ranges=IRanges::IRanges(start=1:10, width=100))
   gr2 <- GRanges(seqnames="chr2", ranges=IRanges::IRanges(start=2:20, width=100))
   gr3 <- GRanges(seqnames="chr1", ranges=IRanges::IRanges(start=seq(1,100,by=25), width=1), 
                  score=rnorm(length(seq(1,100,by=25))))
 
-  server <- epivizrServer::createServer()
-  mgr <- createMgr(server)
+  ms1 <- mgr$add_measurements(gr1, "dev1", send_request=TRUE); msId1 <- ms1$get_id()
+  ms2 <- mgr$add_measurements(gr2, "dev2", send_request=TRUE); msId2 <- ms2$get_id()
+  ms3 <- mgr$add_measurements(gr3, "dev3", send_request=TRUE, type="bp"); msId3 <- ms3$get_id()
+  wait_until(!mgr$.server$has_request_waiting())
   
-  ms1 <- mgr$add_measurements(gr1, "dev1", send_request=FALSE); msId1 <- ms1$get_id()
-  ms2 <- mgr$add_measurements(gr2, "dev2", send_request=FALSE); msId2 <- ms2$get_id()
-  ms3 <- mgr$add_measurements(gr3, "dev3", send_request=FALSE, type="bp"); msId3 <- ms3$get_id()
-
-  expect_equal(mgr$num_datasources(), 3)
+  outputEl <- remDr$findElement(using="id", "add_measurements_output")
+  ms_list <- outputEl$getElementText()[[1]]
+  exp_list <- paste0(sprintf("%s:%s", c(msId1, msId2, msId3), c(msId1, msId2, "score")), collapse=",")
+  cat(ms_list, "\n")
+  cat(exp_list, "\n")
+  expect_equal(ms_list, exp_list)
+  
   mgr$rm_allMeasurements()
-  expect_equal(mgr$num_datasources(), 0)
+  wait_until(!mgr$.server$has_request_waiting())
+  
+  ms_list <- outputEl$getElementText()[[1]]
+  expect_equal(ms_list, "")
 })
 
