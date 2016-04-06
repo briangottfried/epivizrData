9c72dfb8210f1091a42d2e03bb1be09b53c4cc8c
Hector Corrada Bravo <hcorrada@gmail.com>
2016-04-06 12:47:23 -0400

    add examples to docs

--- R/createMgr.R
+++ R/createMgr.R
@@ -3,6 +3,10 @@
 #' @param server An object of class \code{\link{EpivizServer}}
 #' @return An object of class \code{\link{EpivizDataMgr}}
 #' @export
+#' 
+#' @examples
+#' server <- epivizrServer::createServer(port=7123L)
+#' data_mgr <- epivizrData::createMgr(server)
 #'
 createMgr <- function(server = server) {
   if (!is(server, "EpivizServer")) {
