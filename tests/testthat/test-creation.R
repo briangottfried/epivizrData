9eaf565c41164e0cd3a38a3905c1ed2d89d54009
h.corradabravo <h.corradabravo@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-04-20 20:55:04 +0000

    add IRanges to Imports field in DESCRIPTION
    From: Hector Corrada Bravo <hcorrada@gmail.com>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/epivizrData@116618 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test-creation.R
+++ tests/testthat/test-creation.R
@@ -18,7 +18,7 @@ test_that("new creates a proper object", {
 })
 
 test_that("server opening works as expected", {
-  server <- epivizrServer::createServer()
+  server <- epivizrServer::createServer(try_ports=TRUE)
   mgr <- createMgr(server=server)
   expect_true(mgr$is_server_closed())
   
