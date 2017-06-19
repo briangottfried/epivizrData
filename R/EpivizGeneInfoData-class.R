eef9bc215b4d6d95abe51626b40f997fac64caba
Brian Gottfried <briangottfried@me.com>
2017-06-19 15:39:50 -0400

    Fix SQL syntax errors in toMySQL method

--- R/EpivizGeneInfoData-class.R
+++ R/EpivizGeneInfoData-class.R
@@ -94,17 +94,13 @@ EpivizGeneInfoData$methods(
   .get_col_data = function(chr, start, end) {
     return(NULL)
   },
-  export = function(host, unix.socket, user, pass, db_name) {
+  toMySQL = function(host, unix.socket, user, pass, db_name, annotation=NULL) {
     driver <- RMySQL::MySQL()
     connection <- DBI::dbConnect(drv=driver, host=host,
       unix.socket=unix.socket, user=user, pass=pass)
 
-    if (is.null(connection)){
-      stop("Couldn't connect to ", host)
-    }
-
     # INSERT INDEX TO DB
-    index_query <- .self$.insert_index_query(db_name)
+    index_query <- .self$.insert_index_query(db_name, annotation)
     DBI::dbSendQuery(connection,index_query)
 
     # CREATE TABLE IN DB
@@ -121,55 +117,56 @@ EpivizGeneInfoData$methods(
       end=values$end,
       strand=values$strand,
       gene=values$metadata$gene,
-      exon_starts=values$metadata$exon_starts,
-      exon_ends=values$metadata$exon_ends
+      exon_starts=unlist(values$metadata$exon_starts, use.names=FALSE),
+      exon_ends=unlist(values$metadata$exon_ends, use.names=FALSE),
+      stringsAsFactors=FALSE
     )
 
-    SQL_values <- paste0("('",
-      df[1:nrow(df),'chr'], "',",
-      df[1:nrow(df),'start'], ",",
-      df[1:nrow(df),'end'], ",",
-      df[1:nrow(df),'strand'], ",",
-      df[1:nrow(df),'gene'], ",",
-      df[1:nrow(df),'exon_starts'], ",",
-      df[1:nrow(df),'exon_ends'],
-      ")")
+    # BATCH QUERIES
+    insert_queries <- lapply(seq(1, nrow(df), 50),
+      function(index, step, datasource) {
+        if ((nrow(df) - index) < step) {
+          step <- (nrow(df) - index)
+        }
+        batch <- paste0("(",
+           "'", df[index:(index+step),'chr'], "',",
+           df[index:(index+step),'start'], ",",
+           df[index:(index+step),'end'], ",",
+           "'", df[index:(index+step),'strand'], "',",
+           "'", df[index:(index+step),'gene'], "',",
+           "'", df[index:(index+step),'exon_starts'],"',",
+           "'",df[index:(index+step),'exon_ends'], "'",
+           ")", collapse=",")
 
-    insert_queries <- lapply(seq(1, length(SQL_values), 50),
-      function(index, step){
-        batch <- paste(na.omit(SQL_values[index:(index+step)]), collapse=",")
-        return(paste0(
-        "INSERT INTO TABLE `", db_name, ".", datasource, "` ",
-        "(chr, start, end, strand, gene, exon_starts, exon_ends) VALUES ", batch))
-    }, step=49)
+        paste0(
+        "INSERT INTO ", db_name, ".`", datasource, "` ",
+        "(chr, start, end, strand, gene, exon_starts, exon_ends) VALUES ", batch)
+    }, step=49, datasource=.self$get_name())
 
     for (batch in insert_queries) {
-      DBI::dbSendQuery(connection,insert_query)
+      DBI::dbSendQuery(connection, batch)
     }
 
     DBI::dbDisconnect(connection)
 
     invisible()
   },
-  .insert_index_query = function(db_name){
+  .insert_index_query = function(db_name, annotation="NULL") {
     index <- "gene_data_index"
     min <- 0
     max <- 0
-    window_size <- 0
-    annotation <- NULL
     datasource <- .self$get_name()
 
     index_query <- paste0(
-      "INSERT INTO TABLE `", db_name, ".", index,
-      "` VALUES (",
-      "'", datasource, "'",
-      "'", datasource, "'",
-      "'", datasource, "'",
-      "'", datasource, "'",
-      min,
-      max,
-      window_size,
-      annotation, # Annotation col:  (JSON format). for example {"tissue": "colon", "subtype": "tumor"}
+      "INSERT IGNORE INTO ", db_name, ".", index,
+      " VALUES (",
+      "'", datasource, "'", ",",
+      "'", datasource, "'", ",",
+      "'", datasource, "'", ",",
+      "'", datasource, "'", ",",
+      min, ",",
+      max, ",",
+      "'", annotation, "'",
       ")")
 
     return(index_query)
@@ -177,18 +174,17 @@ EpivizGeneInfoData$methods(
   .create_table_query = function(db_name) {
     datasource <- .self$get_name()
     return(paste0(
-      "CREATE TABLE `", db_name, ".", datasource, "` ",
-      "VALUES (",
-      "`id` bigint(20) NOT NULL AUTO_INCREMENT,
+      "CREATE TABLE IF NOT EXISTS ", db_name, ".`", datasource, "` (
+      `id` bigint(20) NOT NULL AUTO_INCREMENT,
       `chr` varchar(20) NOT NULL,
       `start` bigint(20) NOT NULL,
       `end` bigint(20) NOT NULL,
       `gene` varchar(255) DEFAULT NULL,
       `strand` varchar(20) DEFAULT NULL,
-      `exon_starts` longtext,
-      `exon_ends` longtext,
+      `exon_starts` text,
+      `exon_ends` text,
       PRIMARY KEY (`id`,`chr`,`start`),
       KEY `location_idx` (`start`,`end`)
-    ) ENGINE=MyISAM AUTO_INCREMENT=45891354 DEFAULT CHARSET=latin1"))
+    ) ENGINE=MyISAM DEFAULT CHARSET=latin1"))
   }
 )
