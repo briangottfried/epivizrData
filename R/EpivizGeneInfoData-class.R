e861f89ab9a70875a673ef1dcdc43a442a70f765
Brian Gottfried <briangottfried@me.com>
2017-06-20 14:24:52 -0400

    Change toMySQL class structure

--- R/EpivizGeneInfoData-class.R
+++ R/EpivizGeneInfoData-class.R
@@ -94,87 +94,26 @@ EpivizGeneInfoData$methods(
   .get_col_data = function(chr, start, end) {
     return(NULL)
   },
-  toMySQL = function(host, unix.socket, user, pass, db_name, annotation=NULL) {
-    driver <- RMySQL::MySQL()
-    connection <- DBI::dbConnect(drv=driver, host=host,
-      unix.socket=unix.socket, username=user, password=pass)
-
-    # INSERT INDEX TO DB
-    index_query <- .self$.insert_index_query(db_name, annotation)
-    DBI::dbSendQuery(connection,index_query)
-
-    # CREATE TABLE IN DB
-    table_query <- .self$.create_table_query(db_name)
-    DBI::dbSendQuery(connection, table_query)
-
-    # INSERT DATA TO DB
-    all_rows <- .self$get_rows(query=NULL, metadata=c("gene", "exon_starts","exon_ends"))
-    values <- all_rows$values
-
-    df <- data.frame(
-      chr=values$chr,
-      start=values$start,
-      end=values$end,
-      strand=values$strand,
-      gene=values$metadata$gene,
-      exon_starts=unlist(values$metadata$exon_starts, use.names=FALSE),
-      exon_ends=unlist(values$metadata$exon_ends, use.names=FALSE),
-      stringsAsFactors=FALSE
-    )
-
-    # BATCH QUERIES
-    insert_queries <- lapply(seq(1, nrow(df), 50),
-      function(index, step, datasource) {
-        if ((nrow(df) - index) < step) {
-          step <- (nrow(df) - index)
-        }
-        batch <- paste0("(",
-           "'", df[index:(index+step),'chr'], "',",
-           df[index:(index+step),'start'], ",",
-           df[index:(index+step),'end'], ",",
-           "'", df[index:(index+step),'strand'], "',",
-           "'", df[index:(index+step),'gene'], "',",
-           "'", df[index:(index+step),'exon_starts'],"',",
-           "'",df[index:(index+step),'exon_ends'], "'",
-           ")", collapse=",")
-
-        paste0(
-        "INSERT INTO ", db_name, ".`", datasource, "` ",
-        "(chr, start, end, strand, gene, exon_starts, exon_ends) VALUES ", batch)
-    }, step=49, datasource=.self$get_name())
-
-    for (batch in insert_queries) {
-      DBI::dbSendQuery(connection, batch)
+  .mysql_insert_index = function(db_name, annotation) {
+    if (is.null(annotation) || missing(annotation)){
+      annotation <- "NULL"
     }
 
-    DBI::dbDisconnect(connection)
-
-    invisible()
-  },
-  .insert_index_query = function(db_name, annotation="NULL") {
-    index <- "gene_data_index"
-    min <- 0
-    max <- 0
-    datasource <- .self$get_name()
-
-    index_query <- paste0(
-      "REPLACE INTO ", db_name, ".", index,
-      " VALUES (",
-      "'", datasource, "'", ",",
-      "'", datasource, "'", ",",
-      "'", datasource, "'", ",",
-      "'", datasource, "'", ",",
-      min, ",",
-      max, ",",
-      "'", annotation, "'",
-      ")")
-
-    return(index_query)
-  },
-  .create_table_query = function(db_name) {
-    datasource <- .self$get_name()
     return(paste0(
-      "CREATE TABLE IF NOT EXISTS ", db_name, ".`", datasource, "` (
+      "INSERT INTO ", db_name, ".gene_data_index",
+      " VALUES (",
+      "'", .self$get_name(), "'", ",", # measurement_id
+      "'", .self$get_name(), "'", ",", # measurement_name
+      "'", .self$get_name(), "'", ",", # location
+      "'", .self$get_name(), "'", ",", # column_name
+      0, ",", # min
+      0, ",", # max
+      "'", annotation, "'",
+      ")"))
+  },
+  .mysql_create_table = function(db_name) {
+    return(paste0(
+      "CREATE TABLE IF NOT EXISTS ", db_name, ".`", .self$get_name(), "` (
       `id` bigint(20) NOT NULL AUTO_INCREMENT,
       `chr` varchar(20) NOT NULL,
       `start` bigint(20) NOT NULL,
