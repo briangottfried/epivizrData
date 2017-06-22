f750c89616ebd6b3c6a0b2e6fe19b02873e8a0ca
Brian Gottfried <briangottfried@me.com>
2017-06-22 09:47:31 -0400

    Fixed arguments

--- R/utils.R
+++ R/utils.R
@@ -1,17 +1,19 @@
-
+#' Generic as.data.frame method for EpivizData objects
 #' @export
 setMethod ("as.data.frame", signature("EpivizData"),
-  function (x, query=NULL, row.names=NULL, optional=FALSE, ...) {
+  function (x, query=NULL, ...) {
     rows <- x$get_rows(query, metadata=x$get_metadata_columns())
-
     metadata <- as.data.frame(lapply(rows$values$metadata, unlist), ...)
-    rows$values$metadata <- NULL
-    df <- as.data.frame(rows$values)
-    if (nrow(metadata) != 0) df <- data.frame(df, metadata, stringsAsFactors=FALSE)
 
-    cols<- x$.get_col_data(query)
+    rows$values$id <- NULL
+    rows$values$metadata <- NULL
+
+    df <- as.data.frame(rows$values, ...)
+    if (nrow(metadata) != 0) df <- data.frame(df, metadata, ...)
+
+    cols <- x$.get_col_data(query)
     cols_df <- as.data.frame(lapply(cols, function(col) col$values))
-    if (nrow(cols_df) != 0) df <- data.frame(df, cols_df, stringsAsFactors=FALSE)
+    if (nrow(cols_df) != 0) df <- data.frame(df, cols_df, ...)
 
     df
   }
