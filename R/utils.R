7dc91e4d498ec37c21932f66338506d1064761ba
Brian Gottfried <briangottfried@me.com>
2017-06-26 15:52:43 -0400

    Utility function for tranfering data from AH to a MySQL db

--- R/utils.R
+++ R/utils.R
@@ -18,3 +18,42 @@ setMethod ("as.data.frame", signature("EpivizData"),
     df
   }
 )
+
+#' Utility function to import data to a MySQL database from Annotation Hub
+#' @param hub_ids List of Annotation Hub record ids
+#' @param hub Annotation Hub object
+#' @param connection DBIConnection to a MySQL database
+#' @param db_name Name of the database in MySQL database you are importing to
+#' @param ... arguments for toMySQL (annotation, batch)
+#' @export
+ahToMySQL <-  function (hub_ids, hub, connection, db_name, ...) {
+  
+  # if (!is.list(hub_ids)) {
+  #   stop(hub_ids, " must be a list of record ids")
+  # }
+  
+  if (!is(hub, "AnnotationHub")) {
+    stop(hub, " must be an 'AnnotationHub' object")
+  }
+  
+  for (id in hub_ids) {
+    record <- NULL
+    try({
+      record <- hub[[id]]
+      title <- hub[id]$title
+    })
+    
+    if (!is.null(record)) {
+      # TODO: Cleaner way to handle errors for unsupported objects
+      # is it better toc  ms_obj <- NULL
+      try({
+        ms_obj <- epivizrData::register(record)
+        ms_obj$set_name(title)
+      })
+      
+      if (!is.null(ms_obj)) {
+        ms_obj$toMySQL(connection, db_name, ...) 
+      }
+    }
+  }
+}
